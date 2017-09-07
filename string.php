6f518ee071a8d88aefd1d22603f55de1068ac848
-MMK- <masoumiprojects7@gmail.com>
2017-09-07 12:33:17 +0200

    String functie opdracht
    String functie opdracht

--- string.php
+++ string.php
@@ -1,9 +1,12 @@
 <?php
+$string = "dEzE TeKSt wOrDt opgeSchoond ";
 function verbeter($string){
 $string = strtolower($string);
 $string = ucwords($string);
 $string = rtrim($string, " ");
 echo $string;
+}
 
 
+verbeter($string);
 ?>
