37a19a7fe5cdc601210bc7f86318bd4624acac7e
-MMK- <masoumiprojects7@gmail.com>
2017-09-21 09:56:15 +0200

    Edit

--- Upload Image/index.php
+++ Upload Image/index.php
@@ -3,12 +3,11 @@
 <html>
 <body>
 
-
 <center>
 <form action="upload.php" method="post" enctype="multipart/form-data">
     Selecteer een afbeelding om te uploaden:
     <input type="file" name="fileToUpload" id="fileToUpload">
-    <input type="submit" value="Upload Image" name="submit">
+    <input type="submit" value="Afbeelding uploaden" name="submit">
 </form>
 </center>
 
--- Upload Image/upload.php
+++ Upload Image/upload.php
@@ -3,12 +3,12 @@ $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-$fileTpUpload;
 
 
 // Check of het een echte afbeelding is
 if(isset($_POST["submit"])) {
     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+
     if($check !== false) {
         //echo "Bestand is een afbeelding - " . $check["mime"] . ".";
         $uploadOk = 1;
@@ -18,17 +18,20 @@ if(isset($_POST["submit"])) {
     }
 }
 
+
 // Check of bestand al bestaat
 if (file_exists($target_file)) {
     echo "Sorry, Afbeelding bestaat al.";
     $uploadOk = 0;
 }
 
+
 // Check file size
 if ($_FILES["fileToUpload"]["size"] > 500000) {
     echo "Sorry, Afbeelding is te groot.";
     $uploadOk = 0;
 }
+
 
 // alleen afbeeldingen toestaan
 if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
@@ -37,9 +40,11 @@ if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg
     $uploadOk = 0;
 }
 
+
 // Check ff $uploadOk is geset naar 0 door een error
 if ($uploadOk == 0) {
     echo "Uw afbeelding is niet geupload..";
+
 
 // als alles werkt, upload bestand
 } else {
